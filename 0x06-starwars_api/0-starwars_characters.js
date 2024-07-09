64178eff8b427981a533558affe8a64bb5c9896f
chapaedb <chapaeresso1@gmail.com>
2024-07-10 00:04:47 +0300

    Added the file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,28 @@
 const request = require('request');
-request('https://swapi-api.alx-tools.com/api/', function (error, response, body){
-	let result =
+const fetchName = function (arr, i){
+	if(i >= arr.length){
+		return}
+request(arr[i], function (err, response, body){
+	if(err){
+		console.log("Erro", err)
+	}
+
+	
+	else{
+		let data = JSON.parse(body).name
+		console.log(data)
+		fetchName(arr, i + 1)
+	}
+})
 }
+
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body){
+if(error){
+	console.log("Request failed", error);
+}
+if(response.statusCode === 200){
+	const characters = JSON.parse(body).characters;
+	fetchName(characters, 0)
+}
+
+})
