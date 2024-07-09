e29413819e02115a72e872803e9976ebc2d92cec
chapaedb <chapaeresso1@gmail.com>
2024-07-10 00:32:03 +0300

    Adhering to semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,27 @@
 #!/usr/bin/node
+
 const request = require('request');
-const fetchName = function (arr, i){
-	if(i >= arr.length){
-		return}
-request(arr[i], function (err, response, body){
-	if(err){
-		console.log("Erro", err)
-	}
 
-	
-	else{
-		let data = JSON.parse(body).name
-		console.log(data)
-		fetchName(arr, i + 1)
-	}
-})
-}
+const fetchName = function (arr, i) {
+  if (i >= arr.length) {
+    return;
+  }
+  request(arr[i], function (err, response, body) {
+    if (err) {
+      console.log('Error', err);
+    } else {
+      const data = JSON.parse(body).name;
+      console.log(data);
+      fetchName(arr, i + 1);
+    }
+  });
+};
 
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body){
-if(error){
-	console.log("Request failed", error);
-}
-if(response.statusCode === 200){
-	const characters = JSON.parse(body).characters;
-	fetchName(characters, 0)
-}
-
-})
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
+  if (error) {
+    console.log('Request failed', error);
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    fetchName(characters, 0);
+  }
+});
