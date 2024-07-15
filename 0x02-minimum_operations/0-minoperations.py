dfd1354d40a5f218aa67910845502191f70eed4e
chapaedb <chapaeresso1@gmail.com>
2024-07-15 00:02:28 -0700

    Update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+""" Minimum Operations
+    """
 
-def minOperations(n):
-    """Returns the minimum number of ops"""
-    if n <= 1:
+
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
         return 0
-    
-    operations = 0
-    factor = 2
-    
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-    
-    return operations
+    return op
